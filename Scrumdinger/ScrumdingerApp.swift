66edcb48ffab75cb19fa52ec33783c90f48f75cb
DamianoPellegrini <damiano1.pellegrini@gmail.com>
2022-04-11 22:00:30 +0200

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Damiano Pellegrini on 11/04/22.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
